db8296913ac7a58d933925bbd4a2cc8eabf70ce0
firebird777 <codesmell777@gmail.com>
2020-08-29 18:24:17 +0530

    Revert "Revert "added a custom button"
    This reverts commit 60771fa7eafb42aeb66d1ca9f97fc2af7114a7df.
    Adding a new commit

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,26 +1,32 @@
 package application;
 	
 import javafx.application.Application;
-import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
 
 
 public class Main extends Application {
+
 	@Override
-	public void start(Stage primaryStage) {
-		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
+	public void start(Stage primaryStage) throws Exception {
+		Button button = new Button("my first button!");
+		button.setLayoutX(350);
+		button.setLayoutY(350);
+		Pane root = new Pane();
+		Scene scene = new Scene(root,800,800);
+		root.getChildren().add(button);
+		
+		primaryStage.setScene(scene);
+		primaryStage.show();
+		
 	}
 	
-	public static void main(String[] args) {
+	public static void main(String []args)
+	{
 		launch(args);
 	}
+	
 }
