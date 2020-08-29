703743f0efd73d3c592c0c65d9f522a09c60c09e
firebird777 <codesmell777@gmail.com>
2020-08-29 20:17:55 +0530

    created a blank window ,added a name to window

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,45 +1,19 @@
+
 package application;
-	
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.Pane;
+
+import javafx.application.*;
 import javafx.stage.Stage;
-
-
-
-public class Main extends Application {
-
-	@Override
-<<<<<<< HEAD
-	public void start(Stage primaryStage) {
-		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,600);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-=======
-	public void start(Stage primaryStage) throws Exception {
-		Button button = new Button("my first button!");
-		button.setLayoutX(350);
-		button.setLayoutY(350);
-		Pane root = new Pane();
-		Scene scene = new Scene(root,800,800);
-		root.getChildren().add(button);
-		
-		primaryStage.setScene(scene);
-		primaryStage.show();
-		
->>>>>>> db8296913ac7a58d933925bbd4a2cc8eabf70ce0
-	}
-	
+public class Main extends Application{
 	public static void main(String []args)
 	{
 		launch(args);
 	}
-	
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		
+		primaryStage.show();
+		primaryStage.setTitle("NotePad With Tools");
+		
+	}
 }
